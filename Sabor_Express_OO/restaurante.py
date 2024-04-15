7cf87a2ff0924753f65e9d9429959808adab05e6
ViniMata <vinicius.matareli2005@gmail.com>
2024-04-15 16:57:18 -0300

    att

--- Sabor_Express_OO/restaurante.py
+++ Sabor_Express_OO/restaurante.py
@@ -1,15 +1,15 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
-
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Ecxpress', 'Italiana')
 
 
 restaurantes = [restaurante_pizza, restaurante_praca]
 
-print(vars(restaurante_praca))
+
+print(restaurante_praca)
+print(restaurante_pizza)
